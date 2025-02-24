a72882fec62d263ca3ae146dd0ba5dba3bb69e9f
Udbasili <33573587+udabasili@users.noreply.github.com>
2025-02-24 13:00:53 -0400

    updated

--- src/app/feature/projects/components/Project.tsx
+++ src/app/feature/projects/components/Project.tsx
@@ -11,9 +11,9 @@ const imagePaths = [
     'portfolio/sample-portfolio/whax7gbm8huekiqp0kz8',
     'portfolio/sample-portfolio/pjnynvdp79tqobygmzeb',
     'portfolio/sample-portfolio/gi5dk98hts8dkwymsm5m',
-    'portfolio/sample-portfolio/whax7gbm8huekiqp0kz8',
-    'portfolio/sample-portfolio/pjnynvdp79tqobygmzeb',
-    'portfolio/sample-portfolio/gi5dk98hts8dkwymsm5m',
+    'portfolio/sample-portfolio/vibrant_qchhrj',
+    'portfolio/sample-portfolio/cyber-groceries_uqki1y',
+    'portfolio/sample-portfolio/edukate_n405ss',
 ];
 
 
